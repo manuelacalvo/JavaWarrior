bc00c10f631458c83db0aa2def020ff66909bddc
Red4nter <Red4nter@github.com>
2019-07-16 19:24:46 +0100

    lol

--- desktop/src/src/TexturePackaging.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-import com.badlogic.gdx.tools.texturepacker.TexturePacker;
-
-public class TexturePackaging {
-    public static void main(String[] args){
-        TexturePacker.process("Ressources/TilesetGamme/", "assets/", "TilesetGame");
-    }
-}
